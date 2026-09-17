d713b753724202c8d596451575b21497857c824f
EmiGb <emigb2409@gmail.com>
2026-09-17 17:17:14 -0600

    Agregar archivo index.php con estructura HTML básica y estilos

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        h1 {
+            color: blue;
+        }
+        h2 {
+            color: green;
+        }
+        h3 {
+            color: red;
+        }
+        h4 {
+            color: purple;
+        }
+        h5 {
+            color: orange;
+        }
+        h6 {
+            color: pink;
+        }
+
+        div {
+            background-color: #5d0101;
+            padding-left: 25px;
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+
+        div h2 {
+            color: white;
+        }
+
+        div p {
+            color: white;
+            font-style: italic;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hola</h1>
+    <h2>Hola</h2>
+    <h3>Hola</h3>
+    <h4>Hola</h4>
+    <h5>Hola</h5>
+    <h6>Hola</h6>
+
+    <hr>
+
+    <div>
+        <h2>TItulo del parrafo</h2>
+        <p>Bienvenido a mi sitio web</p>
+    </div>
+</body>
+</html>
